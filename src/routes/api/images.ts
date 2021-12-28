3dec777573b0788f0daa0868239ef231bb8ee2f4
ryan efendy <ryanefendy95@gmail.com>
2021-12-27 20:13:18 -0800

    endpoint return pic

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,7 +1,11 @@
-
 import { Router, Request, Response } from 'express';
+import path from 'path';
+
 const images = Router();
-images.get('/', (_req: Request, res: Response) => {
-  res.send('images route');
+images.get('/', (req: Request, res: Response) => {
+    //   console.log(req.query);
+    const {filename } = req.query;
+    res.sendFile(path.resolve(`./src/images/full/${filename}.jpg`));
+    // res.send('images route');
 });
 export default images;
