0728fc1266778c56e7eb9797f8bed6ccfeee5e1b
ryan efendy <ryanefendy95@gmail.com>
2021-12-30 14:11:06 -0800

    add health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,6 @@ routes.get('/', (_req: Request, res: Response) => res.send('home route'));
 
 routes.use('/images', images);
 
+routes.get('/health', (_req: Request, res: Response) => res.send({ message: 'OK' }));
+
 export default routes;
