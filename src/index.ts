b3f9321c9c4d5d52fc7e5951502c7978388bbfa9
ryan efendy <ryanefendy95@gmail.com>
2021-12-30 14:11:13 -0800

    add pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import pinoHttp from 'pino-http';
 import routes from './routes';
+import logger from './util/logger';
 
 const app = express();
 const port = 3000;
 
+app.use(pinoHttp({ logger }));
 app.use('/api', routes);
+
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
