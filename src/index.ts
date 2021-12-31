eef3e54715b5acb7d1ef84c752b9461778b69b73
ryan efendy <ryanefendy95@gmail.com>
2021-12-30 19:41:44 -0800

    add createDirIfNotExist

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from './app';
+import { createDirIfNotExist } from './util';
 
 const port = 3000;
 
+createDirIfNotExist('./src/images/thumb/');
 app.listen(port, () => console.log(`server started at http://localhost:${port}`));
